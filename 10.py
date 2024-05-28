15d6c0219b8e9e056e51b71878d755312d9ba382
Rennan <141781304+Eorennas@users.noreply.github.com>
2024-05-28 11:44:43 -0300

    3

---
+++ 10.py
@@ -0,0 +1,5 @@
+#recebendo dados
+
+nome = input('QUal o seu nome: ')
+
+print(f'o seu nome é {nome}')
---
+++ 9.py
@@ -0,0 +1,8 @@
+# formatando strings com .format
+a = 'Aaaaa'
+b = 'C'
+c = 1.1
+
+formato = 'a={} b={} c={}'.format(a,b,c)
+
+print(formato)
