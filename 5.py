8f4b217507bf5ab09cafdaecd4c52ce2ec4ca1cc
Rennan <141781304+Eorennas@users.noreply.github.com>
2024-05-28 09:43:00 -0300

    2

---
+++ 5.py
@@ -0,0 +1,15 @@
+# exercicio simples de variaveis e tipo
+
+nome = 'Rennan'
+sobrenome = 'Alves Vieira'
+idade = 20
+ano_nascimento = 2003
+maior_de_idade = idade > 18
+altura_metros = 1.81
+
+print('Nome:',nome)
+print('Sobrenome:',sobrenome)
+print('idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
